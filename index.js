af46ff22f6bc908b142af0296361452e96a2efbb
Kishan Gosrani <kishan.gosrani22@gmail.com>
2022-04-14 17:15:33 +0800

    updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const inquirer = require('requirer');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
 
+const app = express();
+// process.env.PORT to allow this app to be deployed in other environments
+const PORT = process.env.PORT||3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//static folder is public folder
+app.use(express.static('public'));
+
+app.use('/api/notes', require('./routes/apiroutes'));
+app.use('/', require('./routes/htmlroutes'));
+
+app.listen(PORT, () => {
+    console.log(`App Listening on PORT ${PORT}`);
+});
